2de9e2642764217ffda54d48321cd19b7dcdeef0
saurabh-gaonkar <saurabhg@axioned.com>
2022-02-09 14:57:31 +0530

    javascript code added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,12 @@
-let a=document.getElementsByTagName('figure');
-a.onclick(alert());
-
+const photo=document.querySelectorAll('.photo');
+for(i=0;i<=photo.length-1;i++){
+    photo[i].addEventListener('click',function(){
+        for(let j=0;j<=photo.length-1;j++){
+            photo[j].style.width="7.1%";
+        }
+        this.style.width="70%";
+    });
+}
 
 
 
