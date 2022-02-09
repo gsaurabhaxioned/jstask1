31c4d5de0999fa49af4fb1ed691a4093835e8e6b
saurabh-gaonkar <saurabhg@axioned.com>
2022-02-09 15:50:31 +0530

    js code for border radius added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,18 @@
 const photo=document.querySelectorAll('.photo');
-for(i=0;i<=photo.length-1;i++){
+const photo_title=document.querySelectorAll('a');
+const photo_source=document.querySelectorAll('img');
+console.log(photo_title);
+for(let i=0;i<=photo.length-1;i++){
     photo[i].addEventListener('click',function(){
         for(let j=0;j<=photo.length-1;j++){
             photo[j].style.width="7.1%";
+            photo_title[j].style.display="none";
+            photo_source[j].style.borderRadius="100px";
+
         }
         this.style.width="70%";
+        photo_title[i].style.display="block";
+        photo_source[i].style.borderRadius="50px";
     });
 }
 
